872ff814e92f6e9e41d8034f1f23f687d3868351
dhruvpalli <dhruvkpalli@gmail.com>
2025-12-04 20:25:28 -0500

    Create utils module for EMS Narrative Triage System
    This module provides utility functions for logging, metrics, visualization, and configuration management. It makes src/utils/ a Python package and will be updated later as utility files are added.

---
+++ src/utils/__init__.py
@@ -0,0 +1,12 @@
+"""
+Utilities module for EMS Narrative Triage System
+
+This module provides utility functions for logging, metrics,
+visualization, and configuration management.
+
+What it does:
+
+Makes src/utils/ a Python package
+Will be updated later as we add utility files (logging, metrics, visualization)
+
+"""
